57bc7bdaceaf32027aeae7678555f8793cc8252e
kaluek98 <105400174+kaluek98@users.noreply.github.com>
2022-06-22 14:38:09 -0600

    colors

---
+++ 2D Project/Assets/Example.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExampleBehaviourScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer> ().material.color = Color.orange;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.violet;
+        }
+    }
+}
