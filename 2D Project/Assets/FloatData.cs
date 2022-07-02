bde6f8bc3349ac228cb03a211a0fb08072e65a8e
Kizzy Kalu <10958653@uvu.edu>
2022-07-02 11:00:14 -0600

    making my own scriptable objects

--- 2D Project/Assets/FloatData.cs
+++ 2D Project/Assets/FloatData.cs
@@ -9,4 +9,27 @@ public class FloatData : ScriptableObject
     {
         value += num;
     }
+
+    public void ReplaceValue(float num)
+    {
+        value = number;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        if (value <= 0)
+        {
+            onZeroEvent.Invoke();
+        } else if (value >= 1)
+        {
+            value = 1;
+        }
+
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(text txt)
+    {
+        txt.text = value.Tostring();
+    }
 }
